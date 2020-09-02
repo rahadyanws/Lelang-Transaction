8bc960090317006ff801b3f93bbed8910bf8ae20
Rahadyan Widhi <rahadyanws@gmail.com>
2020-09-02 12:12:35 +0700

    init application

---
+++ src/main/java/com/rahadyan/lelangtransaction/LelangTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.rahadyan.lelangtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LelangTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LelangTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahadyan/lelangtransaction/LelangTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahadyan.lelangtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LelangTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
